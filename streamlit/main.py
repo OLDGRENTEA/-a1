0b489e555c142c30322fa29a3b2d755eba4deb9c
OLDGRENTEA <113867774+OLDGRENTEA@users.noreply.github.com>
2023-06-06 15:55:32 +0800

    Update main.py

--- streamlit/main.py
+++ streamlit/main.py
@@ -15,6 +15,10 @@ from textrank4zh import TextRank4Sentence
 
 #加载数据集（数据来源：WHO）
 @st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def load_data():
+    data = pd.read_csv('COVID-19-global-data.csv', encoding='gb2312', error_bad_lines=False, sep='\t')
+    return data
+
 
 con = st.container()
 
